54dc9ce170cecb161d24cde5f9f59657c7dd546b
Ahsanbutt6165 <umair@teknotize.com>
2025-04-29 00:28:43 +0500

    appdata get and add api integration done

--- src/api/AppData.tsx
+++ src/api/AppData.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:3000";
+import { apiClient } from "../config";
 
 export const createBadgeWithCSV = async (formValues, file) => {
   const formData = new FormData();
@@ -18,34 +17,28 @@ export const createBadgeWithCSV = async (formValues, file) => {
   // Add file
   formData.append("csvFile", file); // file should be a File object
 
-  const response = await axios.post(
-    `${BASE_URL}/api/create-badge-with-csv`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+  const response = await apiClient.post(`/create-badge-with-csv`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
   return response.data;
 };
 
 export const getAllSubCategoriesWithBadges = async () => {
-  const response = await axios.get(
-    `${BASE_URL}/api/getAllSubCategoriesWithBadges`
-  );
+  const response = await apiClient.get(`/getAllSubCategoriesWithBadges`);
   return response.data;
 };
 
 export const updateBadge = async (id: any, is_locked: any) => {
-  const response = await axios.patch(`${BASE_URL}/api/updatebadge/${id}`, {
+  const response = await apiClient.patch(`/updatebadge/${id}`, {
     is_locked,
   });
   return response.data;
 };
 
 export const deleteBadge = async (id: any) => {
-  const response = await axios.delete(`${BASE_URL}/api/badge/${id}`);
+  const response = await apiClient.delete(`/badge/${id}`);
   return response.data;
 };
