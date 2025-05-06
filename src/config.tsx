a339c9a40aa9995391505e20f2624f5db7ebd20f
Ahsanbutt6165 <umair@teknotize.com>
2025-05-06 20:34:07 +0500

    fix config file

--- src/config.tsx
+++ src/config.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import refreshAccessToken from "./utils/refreshAccessToken";
 import { useAuthStore } from "./store/useAuthStore";
 
-
 // const BASE_URL = "https://app.hvacsimulator.com/api";
 const BASE_URL = "https://hvac-simulator.vercel.app/api";
 // const BASE_URL = "http://localhost:3000/api";
