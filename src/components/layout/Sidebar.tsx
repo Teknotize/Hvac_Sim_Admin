6a37ce20b5d3ebb81aa38749ce68f1dae4690383
Azim Khan <akhan_24@hotmail.com>
2025-05-22 22:40:07 +0500

    profile pop

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { Button, Dialog, DialogPanel, Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import logo from "../../assets/images/logo.png";
 import profileImage from "../../assets/images/profileImageAdmin.png";
 import {
@@ -11,6 +11,7 @@ import {
   MainMenuNavCRM,
   MainMenuNavDistributors,
   MainMenuNavAppData,
+  ProfileEditIcon,
 } from "../svg/icons";
 import { useNavigate } from "react-router-dom";
 import useLogout from "../logout";
@@ -18,6 +19,8 @@ import { useSidebar } from "../../context/SidebarContext";
 import { useEffect, useState } from "react";
 
 import { apiClient } from "../../config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const navigation = [
   { name: "Dashboard", href: "/dashboard", icon: <MainMenuNavDashboard /> },
@@ -45,6 +48,7 @@ export default function Sidebar() {
   const navigate = useNavigate();
   const { isOpen } = useSidebar();
   const [userData, setUserData] = useState<any>("");
+  const [isProfilePopOpen, setisProfilePopOpen] = useState(false);
   console.log("userData", userData);
   const handleLogout = () => {
     logout();
@@ -64,152 +68,221 @@ export default function Sidebar() {
   }, []);
 
   return (
-    <div className={`flex h-full flex-col sidebar ${isOpen ? "active" : ""}`}>
-      <div className="sidebar-inner">
-        <div className="flex h-16 items-center gap-4 p-4 border-b-2 border-gray-800">
-          <a href="/" className="logo">
-            <img src={logo} alt="HVAC Sim Admin" />
-          </a>
+    <>
+      <div className={`flex h-full flex-col sidebar ${isOpen ? "active" : ""}`}>
+        <div className="sidebar-inner">
+          <div className="flex h-16 items-center gap-4 p-4 border-b-2 border-gray-800">
+            <a href="/" className="logo">
+              <img src={logo} alt="HVAC Sim Admin" />
+            </a>
 
-          <Popover className="user-info">
-            <PopoverButton className="block">
-              <UserIcon />
-            </PopoverButton>
-            <PopoverPanel
-              transition
-              anchor="bottom end"
-              className="user-info-popover shadow-xl transition duration-200 ease-in-out data-[closed]:-translate-y-1 data-[closed]:opacity-0"
-            >
-              <div className="user-label border-b-1 border-gray-200 pb-3 mb-3">
-                <p>Signed in as</p>
+            <Popover className="user-info">
+              <PopoverButton className="block">
+                <UserIcon />
+              </PopoverButton>
+              <PopoverPanel
+                transition
+                anchor="bottom end"
+                className="user-info-popover shadow-xl transition duration-200 ease-in-out data-[closed]:-translate-y-1 data-[closed]:opacity-0"
+              >
+                <div className="user-label border-b-1 border-gray-200 pb-3 mb-3">
+                  <p>Signed in as</p>
+                  <p>{userData?.user?.username}</p>
+                </div>
+                <div className="user-menu">
+                  <a href="javascript:void(0)" className="user-menu-item" onClick={() => setisProfilePopOpen(true)}>
+                    <UserMenuIconProfile />
+                    <p>Profile</p>
+                  </a>
+
+                  {/* <Link to="/logout" className="user-menu-item">
+                    <UserMenuIconLogout />
+                    <p>Logout</p>
+                  </Link> */}
+
+                  <button
+                    onClick={handleLogout}
+                    className="user-menu-item"
+                    style={{ width: "100%", cursor: "pointer" }}
+                  >
+                    <UserMenuIconLogout />
+                    <p>Logout</p>
+                  </button>
+                </div>
+              </PopoverPanel>
+            </Popover>
+          </div>
+          <div className="flex justify-center">
+            <div className="user-card">
+              <figure className="user-dp">
+                <img src={profileImage} alt="Umair Farooq" />
+              </figure>
+              <div className="user-card-body">
+                <h4>{userData?.user?.name}</h4>
                 <p>{userData?.user?.username}</p>
               </div>
-              <div className="user-menu">
-                <Link to="/profile" className="user-menu-item">
-                  <UserMenuIconProfile />
-                  <p>Profile</p>
-                </Link>
-
-                {/* <Link to="/logout" className="user-menu-item">
-                  <UserMenuIconLogout />
-                  <p>Logout</p>
-                </Link> */}
-
-                <button
-                  onClick={handleLogout}
-                  className="user-menu-item"
-                  style={{ width: "100%", cursor: "pointer" }}
-                >
-                  <UserMenuIconLogout />
-                  <p>Logout</p>
-                </button>
-              </div>
-            </PopoverPanel>
-          </Popover>
-        </div>
-        <div className="flex justify-center">
-          <div className="user-card">
-            <figure className="user-dp">
-              <img src={profileImage} alt="Umair Farooq" />
-            </figure>
-            <div className="user-card-body">
-              <h4>{userData?.user?.name}</h4>
-              <p>{userData?.user?.username}</p>
             </div>
           </div>
-        </div>
-        <div className="flex-1 overflow-y-auto px-2 py-4">
-          <nav className="mainNav">
-            {navigation.map((item) => {
-              const isActive =
-                location.pathname === item.href ||
-                (item.children &&
-                  item.children.some(
-                    (child) => location.pathname === child.href
-                  ));
-              return (
-                <div
-                  key={item.name}
-                  className={`nav-item-group ${
-                    item.children && isActive ? "active" : ""
-                  }`}
-                >
-                  {item.children ? (
-                    <div
-                      className={`nav-item has-sub-items flex ${
-                        isActive ? "active" : ""
-                      }`}
-                      onClick={
-                        item.children
-                          ? (e) => {
-                              e.preventDefault();
-                              // Toggle active class for items with children
-                              const currentTarget = e.currentTarget;
-                              if (currentTarget.parentElement) {
-                                currentTarget.parentElement.classList.toggle(
-                                  "active"
-                                );
+          <div className="flex-1 overflow-y-auto px-2 py-4">
+            <nav className="mainNav">
+              {navigation.map((item) => {
+                const isActive =
+                  location.pathname === item.href ||
+                  (item.children &&
+                    item.children.some(
+                      (child) => location.pathname === child.href
+                    ));
+                return (
+                  <div
+                    key={item.name}
+                    className={`nav-item-group ${
+                      item.children && isActive ? "active" : ""
+                    }`}
+                  >
+                    {item.children ? (
+                      <div
+                        className={`nav-item has-sub-items flex ${
+                          isActive ? "active" : ""
+                        }`}
+                        onClick={
+                          item.children
+                            ? (e) => {
+                                e.preventDefault();
+                                // Toggle active class for items with children
+                                const currentTarget = e.currentTarget;
+                                if (currentTarget.parentElement) {
+                                  currentTarget.parentElement.classList.toggle(
+                                    "active"
+                                  );
+                                }
                               }
-                            }
-                          : undefined
-                      }
-                    >
-                      <div className="nav-item-inner">
-                        {item.icon}
-                        <span>{item.name}</span>
-                      </div>
-                      {item.children && (
-                        <svg
-                          className="h-4 w-4 sub-item-arrow"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M19 9l-7 7-7-7"
-                          />
-                        </svg>
-                      )}
-                    </div>
-                  ) : (
-                    <Link
-                      to={item.href}
-                      className={`nav-item flex ${isActive ? "active" : ""}`}
-                    >
-                      <div className="nav-item-inner">
-                        {item.icon}
-                        <span>{item.name}</span>
-                      </div>
-                    </Link>
-                  )}
-
-                  {item.children && (
-                    <div className="sub-item-group flex flex-col">
-                      {item.children.map((child) => {
-                        const isChildActive = location.pathname === child.href;
-                        return (
-                          <Link
-                            key={child.name}
-                            to={child.href}
-                            className={`nav-sub-item ${
-                              isChildActive ? "active" : ""
-                            }`}
+                            : undefined
+                        }
+                      >
+                        <div className="nav-item-inner">
+                          {item.icon}
+                          <span>{item.name}</span>
+                        </div>
+                        {item.children && (
+                          <svg
+                            className="h-4 w-4 sub-item-arrow"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
                           >
-                            <span>{child.name}</span>
-                          </Link>
-                        );
-                      })}
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-          </nav>
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M19 9l-7 7-7-7"
+                            />
+                          </svg>
+                        )}
+                      </div>
+                    ) : (
+                      <Link
+                        to={item.href}
+                        className={`nav-item flex ${isActive ? "active" : ""}`}
+                      >
+                        <div className="nav-item-inner">
+                          {item.icon}
+                          <span>{item.name}</span>
+                        </div>
+                      </Link>
+                    )}
+
+                    {item.children && (
+                      <div className="sub-item-group flex flex-col">
+                        {item.children.map((child) => {
+                          const isChildActive = location.pathname === child.href;
+                          return (
+                            <Link
+                              key={child.name}
+                              to={child.href}
+                              className={`nav-sub-item ${
+                                isChildActive ? "active" : ""
+                              }`}
+                            >
+                              <span>{child.name}</span>
+                            </Link>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
+
+
+      <Dialog
+        open={isProfilePopOpen}
+        onClose={() => setisProfilePopOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-black/40 dialog-item">
+          <DialogPanel className="max-w-full w-xl space-y-4 border bg-white p-6 rounded-xl dialog-panel">
+            <Button
+                className="closeBtn"
+                onClick={() => setisProfilePopOpen(false)}
+              >
+                <FontAwesomeIcon
+                  icon={faXmark}
+                />
+              </Button>
+            <div className="dialog-body">
+
+              <div className="profileCard">
+                <figure>
+                  <img src={profileImage} alt="Umair Farooq" />
+                  <ProfileEditIcon />
+                </figure>
+                <div className="profileCard-body">
+                  <h4>{userData?.user?.name}</h4>
+                  <p>{userData?.user?.username}</p>
+                </div>
+              </div>
+
+              <div className="fieldItem">
+                <p>Name</p>
+                <input type="text" className="form-control" placeholder={userData?.user?.name ? "Enter name" : "Add Name"} value={userData?.user?.name} disabled={true} />
+              </div>
+
+              <div className="fieldItem">
+                <p>Email account</p>
+                <input type="text" className="form-control" placeholder={userData?.user?.email ? "Enter email" : "Add Email"} value={userData?.user?.email} />
+              </div>
+
+              <div className="fieldItem">
+                <p>Mobile number</p>
+                <input type="text" className="form-control" placeholder={userData?.user?.phone ? "Enter mobile number" : "Add Phone Number"} value={userData?.user?.phone} /> 
+              </div>
+
+              <div className="fieldItem">
+                <p>Location</p>
+                <input type="text" className="form-control" placeholder={userData?.user?.location ? "Enter location" : "Add Location"} value={userData?.user?.location} />
+              </div>
+
+
+
+            </div>
+            <div className="dialog-footer">
+              <Button
+                className="btn btn-primary"
+                onClick={() => {
+                  setisProfilePopOpen(false);
+                }}
+              >
+                Save Changes
+              </Button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+    </>
   );
 }
