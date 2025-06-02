60934f3c8911f7ed890eaee6a75123ccf4d53d9e
Ahsanbutt6165 <umair@teknotize.com>
2025-06-03 01:00:51 +0500

    fix all users select

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // Enables access on both localhost and network IP
-    port: 5178, // Ensure this is your required port
+    port: 5173, // Ensure this is your required port
     strictPort: true, // Prevents Vite from auto-changing the port
   }
 })
