b2980da614dfd806aad3a1c4712ea9043e1959fe
developer-teknotize <138934274+developer-teknotize@users.noreply.github.com>
2025-06-26 19:23:22 +0500

    resolving empty array bug in contact screen

--- src/pages/crm/Contacts.tsx
+++ src/pages/crm/Contacts.tsx
@@ -249,7 +249,7 @@ const itemsPerPage = useCRMStore((state) => state.itemsPerPage);
       setLoading(true);
       try {
         const response = await apiClient.get("/admin/get-crm-users");
-        console.log("Fetched CRM users:", response);
+        console.log("Fetched CRM users:", response?.data,Array.isArray(response?.data));
         const users = response?.data?.map((user: any) => ({
           ...user,
           tags: user.tags.map((tag: string) =>
